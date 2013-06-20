13765ca92cdf3659f2862c6ff3dd6af5ef570d9a
ddh.huy <ddh.huy@gmail.com>
2013-06-20 15:08:56 +0000

    + [Prototype] Design LoginActivity

---
+++ Prototype/VietHospital/src/com/viet/hospital/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.viet.hospital;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+	}
+
+}
